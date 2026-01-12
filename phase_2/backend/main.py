8341a6c10d5d0576101edfcb5a6c92446fbf20b6
n8x-0 <syedshayan1324@gmail.com>
2026-01-12 12:49:35 +0500

    deployment errors on vercel !

--- phase_2/backend/main.py
+++ phase_2/backend/main.py
@@ -71,4 +71,4 @@ def health_check():
 if __name__ == "__main__":
     logger.info("Starting application")
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=5001)
+    uvicorn.run(app, host="0.0.0.0", port=8001)
